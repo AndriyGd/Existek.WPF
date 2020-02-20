0b65d5b95bada43fbe063379e909d64045ff7b6a
Andrew Gladkyi <andriy.gladkyi@existek.com>
2020-02-20 12:12:45 +0200

    Added project - CommonMVVM

---
+++ CommonMVVM/Common/DelegateCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommonMVVM.Common
+{
+    public class DelegateCommand : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
